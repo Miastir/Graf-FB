b33a2b3f2a735bf8a5155b9061546f99f8dd6628
Miastir <74895074+Miastir@users.noreply.github.com>
2020-12-06 20:28:22 +0700

    v 2.2
    Добавлено меню ввода матрицы

--- Graf-FB/floid.cpp
+++ Graf-FB/floid.cpp
@@ -2,19 +2,27 @@
 
 #include "floid.h"
 
-void floid(int(&a)[6][6]) {
+void floid(int(&matrix)[6][6]) {
+    // заполнения бесконечностями всмето 0 для расчета расстояния
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j < 6; j++) {
+            if (matrix[i][j] == 0 && (i!=j)) { matrix[i][j] = 1000; }
+        }
+    }
+
     for (int k = 0; k < 6; k++)
         for (int i = 0; i < 6; i++)
             for (int j = 0; j < 6; j++)
-                if (a[i][j] > a[i][k] + a[k][j])
-                    a[i][j] = a[i][k] + a[k][j];
+                if (matrix[i][j] > matrix[i][k] + matrix[k][j])
+                    matrix[i][j] = matrix[i][k] + matrix[k][j];
 
     std::cout<< std::endl << " Матрица минимальных длин между вершинами :" << std::endl;
 
     for (int i = 0; i < 6; i++)
     {
         for (int j = 0; j < 6; j++)
-            std::cout << a[i][j] << "\t";
+            std::cout << matrix[i][j] << "\t";
         std::cout << std::endl;
     }
     
